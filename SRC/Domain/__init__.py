b4ba568d09e65b8ae908acdd9d3459851445b310
taif-k <taifk817@gmail.com>
2025-05-03 09:49:50 +0530

    importing obj to different modules

--- SRC/Domain/__init__.py
+++ SRC/Domain/__init__.py
@@ -1,2 +1,4 @@
 from .Validations.user_validations import validation_obj
 from .ReadFile.Read_writefile import operation_obj
+from .FoodMenu.add_items import foodmenu_obj
+from .FoodMenu.remove_items import removemenu_obj
