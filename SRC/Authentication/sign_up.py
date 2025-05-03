a3529c5602bfe725f89209249b6e55d6d3214539
taif-k <taifk817@gmail.com>
2025-05-03 12:13:19 +0530

    default user role = "staff"

--- SRC/Authentication/sign_up.py
+++ SRC/Authentication/sign_up.py
@@ -1,7 +1,6 @@
-import uuid
 import os,sys
 sys.path.append(os.getcwd())
-
+from SRC.Domain import validation_obj,operation_obj
 userdata_path = r"D:\Repositories\indixpert-dec-batch-2024-restaurant-management-system\SRC\Database\registered_users.json"
 error_path = r"D:\Repositories\indixpert-dec-batch-2024-restaurant-management-system\SRC\Log\error_log.txt"
 
@@ -12,14 +11,13 @@ class RestaurantUsers:
 
     def user_signup(self):
         try:
-            from SRC.Domain import validation_obj,operation_obj
             userdict = {}
             userdict["name"] = validation_obj.user_name()
             userdict["email"] = validation_obj.user_email()
             userdict["contact"] = validation_obj.user_contact()
             userdict["address"] = validation_obj.user_address()
             userdict["user_id"] = userdict["name"] + "_" + validation_obj.user_id()
-            userdict["user_role"] = "staff"
+            userdict["user_role"] = validation_obj.user_role()
             userdict["password"] = validation_obj.user_password()
             userdict["joined_date"] = operation_obj.get_errdetails(get_date=True)
             operation_obj.userlist.append(userdict)
