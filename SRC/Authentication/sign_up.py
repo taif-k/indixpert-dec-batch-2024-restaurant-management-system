baa003d2b95a336e92379f1cb30df02acd23c507
taif-k <taifk817@gmail.com>
2025-05-03 16:00:35 +0530

    object renamed

--- SRC/Authentication/sign_up.py
+++ SRC/Authentication/sign_up.py
@@ -27,4 +27,4 @@ class RestaurantUsers:
             operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
             
 
-user_obj = RestaurantUsers(userdata_path,error_path)
+signup_obj = RestaurantUsers(userdata_path,error_path)
