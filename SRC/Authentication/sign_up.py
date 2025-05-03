78aca91daee59f83355fedacc112f83b9d563124
taif-k <taifk817@gmail.com>
2025-05-03 16:52:03 +0530

    path changed

--- SRC/Authentication/sign_up.py
+++ SRC/Authentication/sign_up.py
@@ -1,7 +1,7 @@
 import os,sys
 sys.path.append(os.getcwd())
 from SRC.Domain import validation_obj,operation_obj
-userdata_path = r"D:\Repositories\indixpert-dec-batch-2024-restaurant-management-system\SRC\Database\registered_users.json"
+userdata_path = r"D:\Repositories\indixpert-dec-batch-2024-restaurant-management-system\SRC\Database\registered_staff.json"
 error_path = r"D:\Repositories\indixpert-dec-batch-2024-restaurant-management-system\SRC\Log\error_log.txt"
 
 class RestaurantUsers:
@@ -13,11 +13,11 @@ class RestaurantUsers:
         try:
             userdict = {}
             userdict["name"] = validation_obj.user_name()
+            userdict["user_role"] = validation_obj.user_role()
             userdict["email"] = validation_obj.user_email()
             userdict["contact"] = validation_obj.user_contact()
             userdict["address"] = validation_obj.user_address()
             userdict["user_id"] = userdict["name"] + "_" + validation_obj.user_id()
-            userdict["user_role"] = validation_obj.user_role()
             userdict["password"] = validation_obj.user_password()
             userdict["joined_date"] = operation_obj.get_errdetails(get_date=True)
             operation_obj.userlist.append(userdict)
