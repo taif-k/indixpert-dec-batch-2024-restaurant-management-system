893d1fcd1d2e6d2d21508c6b371abae3802e1f08
taif-k <taifk817@gmail.com>
2025-05-03 15:34:11 +0530

    using single write func for "a" & "w"

--- SRC/Domain/ReadFile/Read_writefile.py
+++ SRC/Domain/ReadFile/Read_writefile.py
@@ -13,17 +13,16 @@ class DataFile:
         self.err_path = err_path
         self.userlist = self.read_file()
 
-    def write_file(self, data = None, path = None):
+    def write_file(self, data = None, path = None,mode = "w",isJson = 1):
         if data == None and path == None:
             data = self.userlist
             path = self.user_path
 
-        with open(path,"w") as file:
-            file.write(json.dumps(data,indent=3))
-
-    def write_error(self,err_data):
-        with open(self.err_path,"a") as file:
-            file.write(f"\n{err_data}")    
+        with open(path,mode) as file:
+            if isJson == 1:
+                file.write(json.dumps(data,indent=3))
+            else:
+                file.write(f"\n{data}")    
 
     def get_errdetails(self,error = None, get_date = False):
         date = datetime.datetime.now()
@@ -50,7 +49,7 @@ class DataFile:
                 user_data = json.load(file)
                 return user_data     
         except Exception as err:
-            self.write_error(self.get_errdetails(err))
+            self.write_file(self.get_errdetails(err),self.err_path,mode="a",isJson=0)
             return []       
 
 operation_obj = DataFile(userdata_path,error_path)
