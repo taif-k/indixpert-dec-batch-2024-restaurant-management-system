61304206f0d16de2e907b600bbac9ae3764bd08e
taif-k <taifk817@gmail.com>
2025-05-03 09:51:27 +0530

    remove item with its uniq id

---
+++ SRC/Domain/FoodMenu/remove_items.py
@@ -0,0 +1,18 @@
+from SRC.Domain import foodmenu_obj,operation_obj
+
+class RemoveFood:
+    def remove_menu(self):
+        while True:
+            food_id = input("Enter food item id to remove from menu: ")
+            for item in foodmenu_obj.foodmenu_list:
+                if item["food_id"] == food_id:
+                    foodmenu_obj.foodmenu_list.remove(item)
+                    break
+
+            remove_item = input("Remove more items: ")
+            if remove_item != "y":
+                break
+
+        operation_obj.write_file(foodmenu_obj.foodmenu_list,foodmenu_obj.food_path)
+
+removemenu_obj = RemoveFood()
