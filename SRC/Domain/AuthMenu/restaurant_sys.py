26bac74bb106bcf10f1a94abeb3480441a521488
taif-k <taifk817@gmail.com>
2025-05-03 15:33:51 +0530

    handling exception

--- SRC/Domain/AuthMenu/restaurant_sys.py
+++ SRC/Domain/AuthMenu/restaurant_sys.py
@@ -1,5 +1,5 @@
 from Authentication import user_obj,login_obj
-
+from SRC.Domain import operation_obj
 
 class Restaurant:
     def __init__(self):
@@ -13,13 +13,17 @@ class Restaurant:
     def authentication_option(self):
         print("\n----Restaurant Managment System----\n")
         while True:
-            self.authentication_menu()
-            authentication_type = int(input("Enter option: "))
-            if authentication_type == 1:
-                user_obj.user_signup()
-            elif authentication_type == 2:
-                login_obj.user_login()
-            elif authentication_type == 0:
-                break
-            else:
-                print("Invalid option")
+            try:
+                self.authentication_menu()
+                authentication_type = int(input("Enter option: "))
+                if authentication_type == 1:
+                    user_obj.user_signup()
+                elif authentication_type == 2:
+                    login_obj.user_login()
+                elif authentication_type == 0:
+                    break
+                else:
+                    print("Invalid option")
+            except Exception as err:
+                print("Choose valid option ")
+                operation_obj.write_file(data = operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
