623d741008b2b3f27bd69e13a9d89b6754071e4d
taif-k <taifk817@gmail.com>
2025-05-05 08:41:48 +0530

    fixed validtion for food name /category

--- SRC/Domain/FoodMenu/add_items.py
+++ SRC/Domain/FoodMenu/add_items.py
@@ -9,16 +9,16 @@ class MenuItem:
     def add_menu(self):
         foodmenu_dict = {}
         while True:
-            foodmenu_dict["food_item"] = input("Enter Food item to add: ")
-            foodmenu_dict["food_type"] = input("Enter Food category: ")
+            foodmenu_dict["food_item"] = input("Enter Food item to add: ").lower()
+            foodmenu_dict["food_type"] = input("Enter Food category: ").lower()
             foodmenu_dict["food_id"] = foodmenu_dict["food_type"] +"_"+ validation_obj.user_id() #ex: lunch_c123
             for s in range(0,3):
                 size = ["small","medium","large"]
 
-                foodmenu_dict[f"{size[s]}_food_size"] = input(f"is serving size {size[s]}/NA: ")
+                foodmenu_dict[f"{size[s]}_food_size"] = input(f"is serving size {size[s]}/NA: ").lower()
                 foodmenu_dict[f"{size[s]}_food_price"] = int(input("Enter item price: "))
 
-            add_item = input("Enter more items: y/n")
+            add_item = input("Enter more items: y/n").lower()
             if add_item != "y":
                 break
 
