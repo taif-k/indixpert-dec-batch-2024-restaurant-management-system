f354086208ddc2891fee4cd57a8f7eb5fa99e6d2
taif-k <taifk817@gmail.com>
2025-05-06 19:04:02 +0530

    added print msg

--- SRC/Domain/Tables/book_table.py
+++ SRC/Domain/Tables/book_table.py
@@ -9,19 +9,24 @@ class Table:
         self.tablelist = operation_obj.read_file(self.alltable_path)
 
     def add_table(self):
-        table =  0
-        while True:
-            add_table = input("Add Table: y/n")
-            if add_table == "n":
-                break
-            elif add_table == "y":
-                table += 1
-                tabledict = {}
-                tabledict["table_no"] = int(f"{table}")
-                tabledict["available_seats"] = 4  
-                self.tablelist.append(tabledict)
-                print("Table added to Restaurant Successfully...")
-
-        operation_obj.write_file(data=self.tablelist,path=self.alltable_path)
+        try:
+            table =  0
+            while True:
+                add_table = input("Add Table: y/n")
+                if add_table == "n":
+                    break
+                elif add_table == "y":
+                    table += 1
+                    tabledict = {}
+                    tabledict["table_no"] = int(f"{table}")
+                    tabledict["available_seats"] = 4  
+                    self.tablelist.append(tabledict)
+                    print("Table added to Restaurant Successfully...")
+                else:
+                    print("\nInvalid option")
+            operation_obj.write_file(data=self.tablelist,path=self.alltable_path)
+        except Exception as err:
+            print("Try again after some time")
+            operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
 
 table_obj = Table(alltable_path) 
