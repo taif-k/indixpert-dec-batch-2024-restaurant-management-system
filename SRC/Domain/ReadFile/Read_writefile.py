ff033eabbadf08c42504ec325d188a4f430b7ae0
taif-k <taifk817@gmail.com>
2025-05-06 19:01:41 +0530

    msg added "print("check file path..")"

--- SRC/Domain/ReadFile/Read_writefile.py
+++ SRC/Domain/ReadFile/Read_writefile.py
@@ -52,6 +52,7 @@ class DataFile:
                 user_data = json.load(file)
                 return user_data     
         except Exception as err:
+            print("check file path..")
             self.write_file(data=self.get_errdetails(err),path=self.err_path,mode="a",isJson=0)
             return []       
 
