584b0c394e87749fd8209fb533cfcc13db6c36ed
taif-k <taifk817@gmail.com>
2025-05-07 17:26:34 +0530

    made update_obj

--- SRC/Domain/__init__.py
+++ SRC/Domain/__init__.py
@@ -2,6 +2,7 @@ from .Validations.user_validations import validation_obj,Foodvalid_obj,paymentid
 from .ReadFile.Read_writefile import operation_obj
 from .FoodMenu.add_items import foodmenu_obj
 from .FoodMenu.remove_items import removemenu_obj
+from .FoodMenu.update_items import updateitem_obj
 from .Tables.book_table import table_obj
 from .FoodOrder.place_order import order_obj
 from .Payment.pay_bill import bill_obj
