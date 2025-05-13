fda1117cbb13601fa0a5aeaf0c9f067b35cd18d3
taif-k <taifk817@gmail.com>
2025-05-13 11:09:37 +0530

    print strings moved in new seperate class

---
+++ SRC/Domain/Validation/print_variables.py
@@ -0,0 +1,26 @@
+
+class PrintMessage:
+    def __init__(self):
+        self.enter_option = "Enter option: "
+        self.invalid_msg = "Choose valid option "
+        self.err_msg = "Resolving issue..Try again after some time"
+        self.email_address = "Enter email address: "
+        self.enter_password = "Enter password: "
+        self.invalid_info = "Invalid Credentials"
+        self.food_added = "\nFood item added to Menu"
+        self.noid_msg = "\nOrder id not found"
+        self.cancelled_msg = "\nOrder Cancelled"
+        self.no_order_msg = "Cannot place order :("
+        self.choose_validtable = "Choose Available Seats/Table"
+        self.book_confirm_msg = "\nBooking confirmed"
+        self.invalidserving_msg = "\nInvalid serving size" 
+        self.noitem_msg = "\nFood Category/Item Not available...Search anything else"
+        self.invalidamount_msg = "\nPaying Amount should be Greater than bill amount"
+        self.notable_msg = "New Table not added"
+        self.tableno = "Table No."
+        self.availableseat = "Available Seats"  
+
+
+print_obj = PrintMessage()
+        
+     
