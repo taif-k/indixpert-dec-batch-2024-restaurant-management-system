ba26d58dcc814338d75af790073bfb452a0eae46
taif-k <taifk817@gmail.com>
2025-05-13 09:11:20 +0530

    fixed updated table list display without re-run

--- SRC/Domain/Table/display_table.py
+++ SRC/Domain/Table/display_table.py
@@ -1,11 +1,13 @@
 from .book_table import Table
 alltable_path = r"D:\Repositories\indixpert-dec-batch-2024-restaurant-management-system\SRC\Database\alltables.json"
+from SRC.Domain.ReadFile import operation_obj
 
 class DisplayTable(Table):
     def __init__(self, tablepath):
         super().__init__(tablepath)
 
     def available_tables(self):
+        self.tablelist = operation_obj.read_file(self.alltable_path)
         print()
         print(f"{"Table No."}     {"Available Seats"}")
         print("------------------------------------")
