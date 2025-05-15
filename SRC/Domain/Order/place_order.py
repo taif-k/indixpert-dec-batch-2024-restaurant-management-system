30a321377aa368a2d2030896f1e64a9e3384b96a
taif-k <taifk817@gmail.com>
2025-05-15 13:40:41 +0530

    changed format

--- SRC/Domain/Order/place_order.py
+++ SRC/Domain/Order/place_order.py
@@ -76,7 +76,7 @@ class PlaceOrder:
             print(f"{searched_item["food_id"]}\t\t{searched_item["food_item"]}\t\t{'-'}\t\t{searched_item["nosize_food_price"]}\t\t")
         else:
             print(f"{searched_item["food_id"]}\t\t{searched_item["food_item"]}\t\t{"1-Small"}\t\t{searched_item["small_food_price"]}\t\t")
-            print(f"{""}\t\t{""}\t\t{"2-Medium"}\t\t{searched_item["medium_food_price"]}\t\t")
+            print(f"{""}\t\t{""}\t\t{"2-Medium"}\t{searched_item["medium_food_price"]}")
             print(f"{""}\t\t{""}\t\t{"3-Large"}\t\t{searched_item["large_food_price"]}\t\t")
 
 
