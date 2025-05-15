e99e627c2004e43abd7f5859acf881c51b91537e
taif-k <taifk817@gmail.com>
2025-05-15 14:00:21 +0530

    updated contact

--- SRC/Domain/Validation/print_variables.py
+++ SRC/Domain/Validation/print_variables.py
@@ -27,7 +27,7 @@ class PrintMessage:
         self.gst = "GST"
         self.totalbill = "Total Bill"
         self.namenot_aplha = "\nName should be in Alphabets"
-        self.invalid_contact = "\nContact should be of 10 digits" 
+        self.invalid_contact = "\nContact should be valid 10 digits" 
         self.invalid_email = "\nEnter valid Email....Ex: taif@mail.com"
         self.password_demo = "\nPassword must be atleast 8 characters long and include @ also"
 
