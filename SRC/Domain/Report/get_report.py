6007627ee716ec016d02937dead327e76c44cc11
taif-k <taifk817@gmail.com>
2025-05-16 09:05:59 +0530

    fixed no attr error

--- SRC/Domain/Report/get_report.py
+++ SRC/Domain/Report/get_report.py
@@ -14,9 +14,32 @@ class ReportData:
 
     def report_type(self):
         print()
+        print("---------REPORT---------")
         print("1 - Orders")
         print("2 - Errors")
         print("0 - Exit")
+
+    def timeline_range(self):
+        try:
+            self.time_range_menu()
+            range_option = int(input("Enter option: "))
+            today = datetime.datetime.now()
+
+            if range_option == 1:
+                time_compare = today + datetime.timedelta(-7)
+            elif range_option == 2:
+                time_compare = today + datetime.timedelta(-30)
+            elif range_option == 3:
+                time_compare = today + datetime.timedelta(-180)
+            elif range_option == 4:
+                time_compare = today + datetime.timedelta(-365)
+            else:
+                print(print_obj.invalid_msg)
+            
+            self.date_differnce = time_compare
+        except Exception as err:
+            print(print_obj.err_msg)
+            operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
 
     def report_option(self):
         while True:
@@ -47,42 +70,15 @@ class ReportData:
 report_obj = ReportData()
 
 class OrderReport(ReportData):
-    def time_range(self):
+    def orders_timeline(self):
         try:
-            self.time_range_menu()
-            range_option = int(input("Enter option: "))
-            today = datetime.datetime.now()
-
-            if range_option == 1:
-                time_compare = today + datetime.timedelta(-7)
-            elif range_option == 2:
-                time_compare = today + datetime.timedelta(-30)
-            elif range_option == 3:
-                time_compare = today + datetime.timedelta(-180)
-            elif range_option == 4:
-                time_compare = today + datetime.timedelta(-365)
-            else:
-                print(print_obj.invalid_msg)
-                
-            timelist = []
+            self.timeline_range()    
+            timelinelist = []
             for bill in bill_obj.bill_list:
-                if bill["order_time"] >= time_compare.strftime("%d/%m/%Y, %H:%M:%S"):
-                    timelist.append(bill)
+                if bill["order_time"] >= self.date_differnce.strftime("%d/%m/%Y, %H:%M:%S"):
+                    timelinelist.append(bill)
 
-            self.display_mode(timelist)
-        except Exception as err:
-            print(print_obj.err_msg)
-            operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
-
-    def orders_type(self):
-        try:
-            print("1 - pay mode Report")
-            print("2 - TimeWise Report")
-            choose_option = int(input("Enter option: : "))
-            if choose_option == 1:
-                self.mode_type()
-            elif choose_option == 2:
-                self.time_range()
+            self.display_mode(timelinelist)
         except Exception as err:
             print(print_obj.err_msg)
             operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
@@ -93,7 +89,7 @@ class OrderReport(ReportData):
         print("2- Card")
         print("3- Upi") 
 
-        mode_option = int(input("Enter option: : "))
+        mode_option = int(input("Enter mode option: : "))
         if mode_option == 1:
             mode = "cash"
         elif mode_option == 2:
@@ -108,11 +104,27 @@ class OrderReport(ReportData):
             if bill["mode"] == mode:
                 modelist.append(bill)
         self.display_mode(modelist)
+
+    def orders_type(self):
+        try:
+            print("1 - pay mode Report")
+            print("2 - TimeWise Report")
+            choose_option = int(input("Enter option: : "))
+            if choose_option == 1:
+                self.mode_type()
+            elif choose_option == 2:
+                self.orders_timeline()
+        except Exception as err:
+            print(print_obj.err_msg)
+            operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
+
 report_order = OrderReport()
 
 class ErrorReport(ReportData):
+
     def errors_type(self):
         print("Errors report........")
+
 report_error = ErrorReport()
 
 
