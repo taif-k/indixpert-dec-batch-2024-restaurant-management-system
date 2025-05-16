ce80166a5501c999b5fcbb8c94f8b889759d5017
taif-k <taifk817@gmail.com>
2025-05-16 08:51:37 +0530

    divided in classes

--- SRC/Domain/Report/get_report.py
+++ SRC/Domain/Report/get_report.py
@@ -14,46 +14,40 @@ class ReportData:
 
     def report_type(self):
         print()
-        print("1 - pay Mode")
-        print("2 - Time wise")
-
-    def mode_menu(self):
-        print()
-        print("1- Cash")
-        print("2- Card")
-        print("3- Upi") 
+        print("1 - Orders")
+        print("2 - Errors")
+        print("0 - Exit")
 
     def report_option(self):
-        self.report_type()
-        report_option = int(input("Enter option: "))
-        if report_option == 1:
-            self.mode_report()
-        elif report_option == 2:
-            self.time_report()
+        while True:
+            self.report_type()
+            report_option = int(input("\nEnter option: "))
+            if report_option == 1:
+                report_order.orders_type()
+            elif report_option == 2:
+                report_error.errors_type()
+            elif report_option == 0:
+                break
+            else:
+                print(print_obj.invalid_msg)
 
-    def mode_report(self):
-            try:
-                self.mode_menu()
-                pay_mode = int(input("Enter option: : "))
-                if pay_mode == 1:
-                    mode = "cash"
-                elif pay_mode == 2:
-                    mode = "upi"
-                elif pay_mode == 3:
-                    mode = "card"
-                else:
-                    print(print_obj.invalid_msg)
+    def display_mode(self, modelist):
+        print("------------------------------------------------------------------------")
+        print("Order ID      Customer Name      Total Amount      Order Time")
+        print("------------------------------------------------------------------------")
 
-                modelist = []
-                for bill in bill_obj.bill_list:
-                    if bill["mode"] == mode:
-                        modelist.append(bill)
-                self.display_mode(modelist)
-            except Exception as err:
-                print(print_obj.err_msg)
-                operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
- 
-    def time_report(self):
+        total = 0
+        for bill in modelist:
+            print(f"{bill["order_id"]}\t\t{bill["customer_name"]}\t\t{bill["total"]}\t\t{bill["order_time"]}")
+            total += bill["total"]
+
+        print("------------------------------------------------------------------------")
+        print(f"Transactions: {len(modelist)}")
+        print(f"Total : Rs {total}")
+report_obj = ReportData()
+
+class OrderReport(ReportData):
+    def time_range(self):
         try:
             self.time_range_menu()
             range_option = int(input("Enter option: "))
@@ -69,32 +63,56 @@ class ReportData:
                 time_compare = today + datetime.timedelta(-365)
             else:
                 print(print_obj.invalid_msg)
-
+                
             timelist = []
             for bill in bill_obj.bill_list:
                 if bill["order_time"] >= time_compare.strftime("%d/%m/%Y, %H:%M:%S"):
                     timelist.append(bill)
 
             self.display_mode(timelist)
-
         except Exception as err:
             print(print_obj.err_msg)
             operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
 
-    def display_mode(self, modelist):
+    def orders_type(self):
+        try:
+            print("1 - pay mode Report")
+            print("2 - TimeWise Report")
+            choose_option = int(input("Enter option: : "))
+            if choose_option == 1:
+                self.mode_type()
+            elif choose_option == 2:
+                self.time_range()
+        except Exception as err:
+            print(print_obj.err_msg)
+            operation_obj.write_file(data=operation_obj.get_errdetails(err),path=operation_obj.err_path,mode="a",isJson=0)
 
-        print("------------------------------------------------------------------------")
-        print("Order ID      Customer Name      Total Amount      Order Time")
-        print("------------------------------------------------------------------------")
+    def mode_type(self):
+        print()
+        print("1- Cash")
+        print("2- Card")
+        print("3- Upi") 
 
-        total = 0
-        for bill in modelist:
-            print(f"{bill["order_id"]}\t\t{bill["customer_name"]}\t\t{bill["total"]}\t\t{bill["order_time"]}")
-            total += bill["total"]
+        mode_option = int(input("Enter option: : "))
+        if mode_option == 1:
+            mode = "cash"
+        elif mode_option == 2:
+            mode = "upi"
+        elif mode_option == 3:
+            mode = "card"
+        else:
+            print(print_obj.invalid_msg)
 
-        print("------------------------------------------------------------------------")
-        print(f"Transactions: {len(modelist)}")
-        print(f"Total : Rs {total}")
-    
-report_obj = ReportData()
+        modelist = []
+        for bill in bill_obj.bill_list:
+            if bill["mode"] == mode:
+                modelist.append(bill)
+        self.display_mode(modelist)
+report_order = OrderReport()
+
+class ErrorReport(ReportData):
+    def errors_type(self):
+        print("Errors report........")
+report_error = ErrorReport()
+
 
