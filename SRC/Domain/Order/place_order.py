a71effc12a39fabbae743588e4db6e371c8d2ab1
Taif <taifk817@gmail.com>
2025-05-27 21:54:46 +0530

    Update place_order.py

--- SRC/Domain/Order/place_order.py
+++ SRC/Domain/Order/place_order.py
@@ -122,6 +122,7 @@ class PlaceOrder:
 
         print("-------------------------------------")
 
+    # "order_placed" key in order placed.json
     def orders_list(self):
         try:
             self.orderlist = []
